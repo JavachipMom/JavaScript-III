c1cb4fc7ff6f7a434c230b6ce013ee909325a559
JavachipMom <Nicolesp4ever@gmail.com>
2019-10-14 17:19:50 -0500

    Finished doing my prototypes file besides the stretch goals..

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
+* 1. Global binding is refering to the window or object in global scope..
+* 2. Implicit binding is refering to a suggested implication of what it thinks 'this' is referring to.
+* 3. new binding is 
 * 4. 
 *
 * write out a code example of each explanation above
